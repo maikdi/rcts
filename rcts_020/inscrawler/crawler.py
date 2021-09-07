ebcc4380f7b09b862174848fb87da6825b331df0
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2021-09-07 23:46:31 +0700

    version 0.2.0

---
+++ rcts_020/inscrawler/crawler.py
@@ -0,0 +1,162 @@
+from __future__ import unicode_literals
+
+import glob
+import os
+import time
+from builtins import open
+from time import sleep
+
+from .browser import Browser
+from .exceptions import RetryException
+from .fetch import fetch_details
+from .utils import instagram_int
+from .utils import retry
+
+
+class Logging(object):
+    PREFIX = "instagram-crawler"
+
+    def __init__(self):
+        try:
+            timestamp = int(time.time())
+            self.cleanup(timestamp)
+            self.logger = open("/tmp/%s-%s.log" % (Logging.PREFIX, timestamp), "w")
+            self.log_disable = False
+        except Exception:
+            self.log_disable = True
+
+    def cleanup(self, timestamp):
+        days = 86400 * 7
+        days_ago_log = "/tmp/%s-%s.log" % (Logging.PREFIX, timestamp - days)
+        for log in glob.glob("/tmp/instagram-crawler-*.log"):
+            if log < days_ago_log:
+                os.remove(log)
+
+    def log(self, msg):
+        if self.log_disable:
+            return
+
+        self.logger.write(msg + "\n")
+        self.logger.flush()
+
+    def __del__(self):
+        if self.log_disable:
+            return
+        self.logger.close()
+
+
+class InsCrawler(Logging):
+    URL = "https://www.instagram.com"
+    RETRY_LIMIT = 10
+
+    def __init__(self, has_screen=False):
+        super(InsCrawler, self).__init__()
+        self.browser = Browser(has_screen)
+        self.page_height = 0
+        self.login()
+
+    def _dismiss_login_prompt(self):
+        ele_login = self.browser.find_one(".Ls00D .Szr5J")
+        if ele_login:
+            ele_login.click()
+
+    def login(self):
+        browser = self.browser
+        url = "%s/accounts/login/" % (InsCrawler.URL)
+        browser.get(url)
+        u_input = browser.find_one('input[name="username"]')
+        u_input.send_keys('testing_eksternal_jm')
+        p_input = browser.find_one('input[name="password"]')
+        p_input.send_keys('testingeksternal')
+
+        login_btn = browser.find_one(".L3NKy")
+        login_btn.click()
+
+        @retry()
+        def check_login():
+            if browser.find_one('input[name="username"]'):
+                raise RetryException()
+
+        check_login()
+
+    def get_user_profile(self, username):
+        browser = self.browser
+        url = "%s/%s/" % (InsCrawler.URL, username)
+        browser.get(url)
+        name = browser.find_one(".rhpdm")
+        desc = browser.find_one(".-vDIg span")
+        photo = browser.find_one("._6q-tv")
+        statistics = [ele.text for ele in browser.find(".g47SY")]
+        post_num, follower_num, following_num = statistics
+        return {
+            "name": name.text,
+            "desc": desc.text if desc else None,
+            "photo_url": photo.get_attribute("src"),
+            "post_num": post_num,
+            "follower_num": follower_num,
+            "following_num": following_num,
+        }
+
+    def get_user_posts(self, username, number=None):
+        user_profile = self.get_user_profile(username)
+        if not number:
+            number = instagram_int(user_profile["post_num"])
+
+        self._dismiss_login_prompt()
+
+        return self._get_posts(number)
+
+    def _get_posts(self, num):
+        """
+            To get posts, we have to click on the load more
+            button and make the browser call post api.
+        """
+        TIMEOUT = 600
+        browser = self.browser
+        key_set = set()
+        posts = []
+        pre_post_num = 0
+        wait_time = 1
+
+        def start_fetching(pre_post_num, wait_time):
+            ele_posts = browser.find(".v1Nh3 a")
+            for ele in ele_posts:
+                key = ele.get_attribute("href")
+                if key not in key_set:
+                    dict_post = {"key": key}
+                    ele_img = browser.find_one(".KL4Bh img", ele)
+                    dict_post["caption"] = ele_img.get_attribute("alt")
+
+                    dict_post["img_url"] = ele_img.get_attribute("src")
+
+                    print(dict_post["caption"])
+
+                    fetch_details(browser, dict_post)
+
+                    key_set.add(key)
+                    posts.append(dict_post)
+
+                    if len(posts) == num:
+                        break
+
+            if pre_post_num == len(posts):
+                sleep(wait_time)
+                wait_time *= 2
+                browser.scroll_up(300)
+            else:
+                wait_time = 1
+
+            pre_post_num = len(posts)
+            browser.scroll_down()
+
+            return pre_post_num, wait_time
+
+        while len(posts) < num and wait_time < TIMEOUT:
+            post_num, wait_time = start_fetching(pre_post_num, wait_time)
+            pre_post_num = post_num
+
+            loading = browser.find_one(".W1Bne")
+            if not loading and wait_time > TIMEOUT / 2:
+                break
+
+        return posts[:num]
